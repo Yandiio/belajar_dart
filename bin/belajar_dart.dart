00cc9d226f05d6a787a9ada7e3cd5a799d382862
m.yandi.f <spaceship.launcher@gmail.com>
2022-06-01 12:03:01 +0700

    Initial commit

---
+++ bin/belajar_dart.dart
@@ -0,0 +1,7 @@
+import 'package:belajar_dart/belajar_dart.dart' as belajar_dart;
+
+// fungsi [main] akan menjalankan program
+// di dalam fungsi main, terdapat string bertuliskan "Hello World"
+void main(List<String> arguments) {
+  print('Hello world: ${belajar_dart.calculate()}!');
+}
---
+++ bin/variable.dart
@@ -0,0 +1,7 @@
+void main() {
+  var greetings = "Hello Dart!";
+
+  var myAge;
+  // myAge = 20;
+  print(myAge);
+}
